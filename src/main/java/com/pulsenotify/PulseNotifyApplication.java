e6f9046cdaa16a0f49839cbec922156f17870a14
Rohit <rohitbishtt69@gmail.com>
2026-06-05 13:36:30 +0530

    Project Setup

---
+++ src/main/java/com/pulsenotify/PulseNotifyApplication.java
@@ -0,0 +1,12 @@
+package com.pulsenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseNotifyApplication.class, args);
+    }
+}
